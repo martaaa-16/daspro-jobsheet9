1ded0aff1891f9d397f85e4c75f8b2b102353b99
martaaa-16 <martaprama@gmail.com>
2024-10-31 16:40:25 +0700

    1

--- SearchNilai15.java
+++ SearchNilai15.java
@@ -4,24 +4,26 @@ public class SearchNilai15 {
         Scanner sc = new Scanner(System.in);
         
         int jmlMhs = 0;
-        int[] nilaiMhs = new int[jmlMhs];
-        int key = 78;
-        int hasil = 0;
-
         System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
-        int banyak = sc.nextInt();
+        jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
+        int key = 85, hasil = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
             for (int i = 1; i < nilaiMhs.length; i++) {
-                if (key == nilaiMhs[i]) {
-                    hasil = i;
-                }
+                System.out.println("Masukkan nilai yang ingin dicari: " + key);
+            if (key == nilaiMhs[i]) {
+                System.out.println();
+                System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+                hasil = i;
+            } else {
+                System.out.println();
+                System.out.println("Nilai yang dicari tidak ditemukan");
+                break;
+            }
         }
-        System.out.println("Masukkan nilai yang ingin dicari: " + key);
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
     }
 }
--- Tugas1.java
+++ Tugas1.java
@@ -3,32 +3,38 @@ import java.util.Scanner;
 public class Tugas1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[5];
-        double tertinggi = 0, terendah = 100;
-        float totalNilai=0, rataNilai;
 
-        for (int i = 1; i <= 5; i++) {
-            i = 1;
-            while (i <= 3) {
-                totalNilai = 0;
-                System.out.println("Input nilai mahasiswa ke-" + i);
-                for (int j = 1; j <= 5; j++) {
-                    System.out.print("Nilai ke-" + j + " = ");
-                    nilaiMhs[i] = sc.nextInt();
-                    totalNilai += nilaiMhs[i];
-                }
-                rataNilai = totalNilai/5;
-                i++;
-                System.out.println("Rata-rata nilai mahasiswa ke-" + i + " adalah " + rataNilai);
-            if (rataNilai > tertinggi) {
-                tertinggi = rataNilai;
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jmlMhs = sc.nextInt();
+        
+        int[] nilaiMhs = new int[jmlMhs];
+        
+        System.out.print("Masukkan nilai mahasiswa ke-1: ");
+        nilaiMhs[0] = sc.nextInt();
+        int tertinggi = nilaiMhs[0], terendah = nilaiMhs[0], totalNilai = nilaiMhs[0];
+
+        for (int i = 1; i < jmlMhs; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMhs[i] = sc.nextInt();
+            totalNilai += nilaiMhs[i];
+            if (nilaiMhs[i] > tertinggi) {
+                tertinggi = nilaiMhs[i];
             }
-            if (rataNilai < terendah) {
-                terendah = rataNilai;
+            if (nilaiMhs[i] < terendah) {
+                terendah = nilaiMhs[i];
             }
         }
-        System.out.println("Nilai tertinggi: " + tertinggi);
-        System.out.println("Nilai terendah: " + terendah); 
+
+        double rata2 = (double) totalNilai + jmlMhs;
+
+        System.out.println("Daftar nilai mahasiswa: ");
+
+        for (int i = 0; i < jmlMhs; i++) {
+            System.out.println("Nailai mahasiswa ke-" + (i + 1) + ": " + nilaiMhs[i]);
         }
+
+        System.out.println("Nilai mahasiswa yang tertinggi: " + tertinggi);
+        System.out.println("Nilai mahasiswa yang terendah: " + terendah); 
+        System.out.println("Rata-rata nilai mahasiswa: " + rata2);       
     }
 }
