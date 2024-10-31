1b2f61d02862d9c14c37e3d36e9f640afe211352
martaaa-16 <martaprama@gmail.com>
2024-10-31 17:09:51 +0700

    Tugas 2

--- Tugas2.java
+++ Tugas2.java
@@ -3,6 +3,31 @@ public class Tugas2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        System.out.print("Masukkan jumlah pesanan: ");
+        int jmlPesanan = sc.nextInt();
+        sc.nextLine();
+
+        String[] namaPesanan = new String[jmlPesanan];
+        double[] hargaPesanan = new double[jmlPesanan];
         
+        for (int i = 0; i < jmlPesanan; i++) {
+            System.out.print("Masukkan nama makanan/minuman untuk pesanan " + (i + 1) + ": ");
+            namaPesanan[i] = sc.nextLine();
+
+            System.out.print("Masukkan harga untuk " + namaPesanan[i] + ": ");
+            hargaPesanan[i] = sc.nextDouble();
+            sc.nextLine();
+        }
+
+        double totalBiaya = 0;
+        for (double harga : hargaPesanan) {
+            totalBiaya += harga;
+        }
+
+        System.out.println("Daftar pesanan: ");
+        for (int i = 0; i < jmlPesanan; i++) {
+            System.out.println((i + 1) + ". " + namaPesanan[i] + " - Rp " + hargaPesanan[i]);
+        }
+        System.out.println("Total biaya: Rp " + totalBiaya);
     }
 }
